8ec2c29b62dcbbc9c3b45d1f3b51eb419739894c
Galadriel Cho <42721783+galadrielcho@users.noreply.github.com>
2022-06-06 08:24:26 -0400

    made static homepage, working on routing

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile('./src/html/home.html', {root: __dirname })
+    res.sendFile('./src/html/index.html', {root: __dirname })
 });
 
 server.listen(3000, () => {
