8505f9f86e6141793dab84cb6b5a270cab762bb0
Galadriel Cho <42721783+galadrielcho@users.noreply.github.com>
2022-06-06 17:36:28 -0400

    room manager

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 var path = require('path');
-const http = require('http');
+const http = require('http ');
 const server = http.createServer(app);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+let io = require('socket.io' )
 
 
 app.use(express.static('public'));
@@ -13,6 +19,3 @@ app.use(express.static('public'));
 var indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,21 @@
 var express = require('express');
-var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Tic Tac Toe Home', style: "index" });
-});
+module.exports = function(io) {
+  var router = express.Router();
 
-router.get('/singleplayer', function(req, res, next) {
-  res.render('board', { title: 'Singleplayer', style: "board", js: "Singleplayer"});
-});
+  router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Tic Tac Toe Home', style: "index" });
+  });
 
-router.get('/multiplayer', function(req, res, next) {
-  res.render('board', { title: 'Multiplayer', style: "board", js: "Multiplayer"});
-});
+  router.get('/singleplayer', function(req, res, next) {
+    res.render('board', { title: 'Singleplayer', style: "board", js: "Singleplayer"});
+  });
 
+  router.get('/multiplayer', function(req, res, next) {
+    
+    res.render('board', { title: 'Multiplayer', style: "board", js: "Multiplayer"});
+  });
 
-module.exports = router;
+  return router;
+
+}
