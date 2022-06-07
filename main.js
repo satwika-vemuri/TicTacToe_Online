f70da4596b8146ebf2594b468f29a550c8c2d27e
Galadriel Cho <42721783+galadrielcho@users.noreply.github.com>
2022-06-06 20:13:28 -0400

    Typo fixed!

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const app = express();
 var path = require('path');
-const http = require('http ');
+const http = require('http');
 const server = http.createServer(app);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+
 
 let io = require('socket.io' )
 
 
 app.use(express.static('public'));
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/index')(io);
 app.use('/', indexRouter);
 
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
